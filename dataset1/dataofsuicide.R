e07f2beea510af13921ac7b60a84df7ebc0a5ed5
amitalas98 <55826724+amitalas98@users.noreply.github.com>
2019-11-04 00:10:53 -0800

    Delete dataofsuicide.R

--- dataset1/dataofsuicide.R
+++
@@ -1,8 +0,0 @@
-data <- read.csv("./suicide_rate_overview_1985_to_2016.csv", stringsAsFactors = FALSE)
-
-library(dplyr)
-
-united_states <- data %>% 
-  filter(country %in% "United States") %>% 
-  group_by(sex)
-  
